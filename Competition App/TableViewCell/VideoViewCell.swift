1b50b94bba788c7cc53ed352673f24f6695b24db
Ahzam Ghauri <ahzambaba2@gmail.com>
2020-03-26 12:51:31 +0500

    first commit

---
+++ Competition App/TableViewCell/VideoViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoViewCell.swift
+//  Competition App
+//
+//  Created by iOS App Developer -APP on 3/25/20.
+//  Copyright © 2020 Mohammad Zawwar-APP. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
